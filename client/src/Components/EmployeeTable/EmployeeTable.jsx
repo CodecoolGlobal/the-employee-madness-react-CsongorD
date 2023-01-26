918ae28b67169211ae3f28aecb34fbdf00c3d3f7
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2023-01-26 12:02:58 +0100

    add filter feature

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -1,36 +1,52 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./EmployeeTable.css";
 
-const EmployeeTable = ({ employees, onDelete }) => (
-  <div className="EmployeeTable">
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Level</th>
-          <th>Position</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {employees.map((employee) => (
-          <tr key={employee._id}>
-            <td>{employee.name}</td>
-            <td>{employee.level}</td>
-            <td>{employee.position}</td>
-            <td>
-              <Link to={`/update/${employee._id}`}>
-                <button type="button">Update</button>
-              </Link>
-              <button type="button" onClick={() => onDelete(employee._id)}>
-                Delete
-              </button>
-            </td>
+const EmployeeTable = ({ employees, onDelete }) => {
+  const [value, setValue] = useState("");
+  const newEmployees = employees.filter((employee) =>
+    employee.position.includes(value) || employee.level.includes(value)
+      ? employee
+      : 0
+  );
+  return (
+    <div className="EmployeeTable">
+      <input
+        placeholder="Search by Position or Level"
+        name={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      ></input>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Level</th>
+            <th>Position</th>
+            <th />
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+        </thead>
+        <tbody>
+          {newEmployees.map((employee) => (
+            <tr key={employee._id}>
+              <td>{employee.name}</td>
+              <td>{employee.level}</td>
+              <td>{employee.position}</td>
+              <td>
+                <Link to={`/update/${employee._id}`}>
+                  <button type="button">Update</button>
+                </Link>
+                <button type="button" onClick={() => onDelete(employee._id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default EmployeeTable;
