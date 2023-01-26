7f8f518e2acc7d6363ca574b459b803621a6d1e7
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2023-01-26 14:06:33 +0100

    add arrangeer filter

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -1,14 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./EmployeeTable.css";
 
 const EmployeeTable = ({ employees, onDelete }) => {
+  const [employeeData, setEmployeeData] = useState(employees);
   const [value, setValue] = useState("");
-  const newEmployees = employees.filter((employee) =>
+  const [nameToggle, setNameToggle] = useState(true);
+  const [levelToggle, setLevelToggle] = useState(true);
+  const [positionToggle, setPositionToggle] = useState(true);
+
+  let filteredEmployees = employeeData.filter((employee) =>
     employee.position.includes(value) || employee.level.includes(value)
       ? employee
       : 0
   );
+
+  function nameArranger(name, level, position) {
+    if (name) {
+      nameToggle
+        ? setEmployeeData(employees.sort((a, b) => (a.name > b.name ? 1 : -1)))
+        : setEmployeeData(employees.sort((a, b) => (a.name > b.name ? -1 : 1)));
+    } else if (level) {
+      levelToggle
+        ? setEmployeeData(
+            employees.sort((a, b) => (a.level > b.level ? 1 : -1))
+          )
+        : setEmployeeData(
+            employees.sort((a, b) => (a.level > b.level ? -1 : 1))
+          );
+    } else if (position) {
+      positionToggle
+        ? setEmployeeData(
+            employees.sort((a, b) => (a.position > b.position ? 1 : -1))
+          )
+        : setEmployeeData(
+            employees.sort((a, b) => (a.position > b.position ? -1 : 1))
+          );
+    }
+  }
+
   return (
     <div className="EmployeeTable">
       <input
@@ -21,14 +51,41 @@ const EmployeeTable = ({ employees, onDelete }) => {
       <table>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Level</th>
-            <th>Position</th>
+            <th>
+              <button
+                onClick={() => {
+                  setNameToggle(nameToggle ? false : true);
+                  nameArranger(true, false, false);
+                }}
+              >
+                Name
+              </button>
+            </th>
+            <th>
+              <button
+                onClick={() => {
+                  setLevelToggle(levelToggle ? false : true);
+                  nameArranger(false, true, false);
+                }}
+              >
+                Level
+              </button>
+            </th>
+            <th>
+              <button
+                onClick={() => {
+                  setPositionToggle(positionToggle ? false : true);
+                  nameArranger(false, false, true);
+                }}
+              >
+                Postition
+              </button>
+            </th>
             <th />
           </tr>
         </thead>
         <tbody>
-          {newEmployees.map((employee) => (
+          {filteredEmployees.map((employee) => (
             <tr key={employee._id}>
               <td>{employee.name}</td>
               <td>{employee.level}</td>
